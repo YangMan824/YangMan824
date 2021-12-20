919567fc5c74876545da56dd322ab663cac7153b
sonder_ym@163.com <sonder_ym@163.com>
2021-12-20 13:57:24 +0800

    Site updated: 2021-12-20 13:57:23

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "/funny.ico");
-        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+        document.title = 'w(ﾟДﾟ)w不要走!再看看嘛!';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
+        document.title = '♪(´▽｀) 欢迎回来！' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
